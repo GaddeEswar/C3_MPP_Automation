81b7208d4ddae7d5e7efcf1318f9e694fb449a31
PoornaKumar <poorna.kumar@grlps.com>
2026-08-10 19:23:45 +0530

    Fun_Not_packets, ReplaceRP_8.4.33, Fun_PktReponses

--- Scripts/OfflineValidationModules/C3TPR/CommonHelper.py
+++ Scripts/OfflineValidationModules/C3TPR/CommonHelper.py
@@ -207,7 +207,7 @@ class CommonCTSChecks:
                 if count%3==2:
                     RP8=self.PktMethod.GetPacketDetails(packet="8 bit Received Power", limit=[id,self.Flow_limit[1]])
                     if len(RP8)>2:
-                        res.append([f'TPR sent 8-Bit Received_Power packet at {{{RP8[2]}}} after 3 RP packets','Pass'])
+                        res.append([f'TPR sent 8-Bit Received_Power packet at index@ {{RP8[2]}} after 3 RP packets','Pass'])
                         id=RP8[2]+1
                         RP8Check=True
                         last_RP8=RP8
@@ -1133,12 +1133,13 @@ class CommonCTSChecks:
             if len(ExpectedPacket_Details)>2:
                 pktCount+=1
                 if pktCount> Check['PktsCount']:
+                    #add (If second packet sent then timer of 2nd packet and it's result)
                     res.append([f'TPR sent the {DataPacket} data packet {pktCount} times. Expected: Not more than {Check['PktsCount']} times','Fail'])
                     break
                 id=ExpectedPacket_Details[2]+1
             else:id+=1
         if pktCount <= Check['PktsCount']: 
-            if Check['PktsCount']==1:res.append([f'TPR did not sent the {DataPacket} data packet atleast one time.','Pass'])
+            if Check['PktsCount']==1:res.append([f'TPR can not send second {DataPacket} data packet.','Pass'])
             else:res.append([f'TPR did not sent the {DataPacket} data packet more than {Check['PktsCount']} times.','Pass'])
                         
         return res
@@ -1364,8 +1365,8 @@ class CommonCTSChecks:
             CE2=self.PktMethod.GetPacketDetails(packet=Check['Pkt'][0],value=Check['Pkt'][1], limit=[CE[2]+1,self.Flow_limit[1]])
             if len(CE2)>2:
                 Tinterval=round((CE2[0]-CE[0])*1000,2)
-                res.append([f'Measured timing from {CE[0]} at index@ {CE[2]} to {CE2[0]} at index@ {CE2[2]} is {Tinterval}mS Limit: {Check['Limit']}', 'Inconclusive' if Tinterval < Check['Limit'][0]-Check['Tolerance'] or Tinterval > Check['Limit'][1]+Check['Tolerance'] else'Pass'])
-            else:res.append([f'Test did not found next {Check['Pkt'][0]} data packet','Inconclusive'])
+                res.append([f'Measured timing from {round(CE[0],3)}_sec at index@ {CE[2]} to {round(CE2[0],3)}_sec at index@ {CE2[2]} is {Tinterval}_mS Limit: {Check['Limit']}', 'Inconclusive' if Tinterval < Check['Limit'][0]-Check['Tolerance'] or Tinterval > Check['Limit'][1]+Check['Tolerance'] else'Pass'])
+            else:res.append([f'TPR sent only one {Check['Pkt'][0]} data packet','Fail'])
         else:res.append([f'Test did not entered PT phase','Inconclusive'])
 
         return res
@@ -1374,11 +1375,17 @@ class CommonCTSChecks:
         res=[]
         self.Flow_limit = flows[flwID]['Limit']
         PKT= str(f'{Check['Pkt'][0]}{'_' + Check['Pkt'][1].replace('{','').replace('}','').replace(':','_') if Check['Pkt'][1] is not None else ''}')
+        Pkt_count= 0
+        count=Check.get('PktsCount')
         id=self.Flow_limit[0]
         while id < self.Flow_limit[1]: 
             Pkt=self.PktMethod.GetPacketDetails(packet=Check['Pkt'][0],value=Check['Pkt'][1] ,limit=[id,self.Flow_limit[1]])
+            
             if len(Pkt)>2:
                 #Check the Response defined in CTS Checks
+                Pkt_count += 1
+                id=Pkt[2]+1
+
                 resp=self.PktResponse(Pkt[2]+1,self.Flow_limit[1])
                 if resp is None and  Check['response'] is None: res.append([f'PTx did not sent any response for {PKT} data packet at index@ {Pkt[2]}','Pass'])
                 else:
@@ -1392,6 +1399,32 @@ class CommonCTSChecks:
                         else:res.append([f'PTx responded with {resp[0]} for {PKT} data packet at index@ {Pkt[2]}','Fail'])
                 id=Pkt[2]+1
             else:id+=1
+        if count is not None: res.append([f'TPR sent {PKT} data packet {Pkt_count} times, Expected at least {Check['PktsCount']} times', 'Pass' if Pkt_count >= Check['PktsCount'] else 'Fail'])
+
+        return res
+
+    def ReplaceRP(self,CTSCheck,Check,flows,flwID):
+        res=[]
+        self.Flow_limit = flows[flwID]['Limit']
+        pktcount=0
+        s1 = self.PktMethod.GetPacketDetails(packet="Test_Status", value="Execution_Started", Type="TesterMsg", limit=[0, len(self.file_list)-1])
+        s2 = self.PktMethod.GetPacketDetails(packet="Test_Status", value="Test_Stop", Type="TesterMsg", limit=[0, len(self.file_list)])
+
+        if len(s1) > 2 and len(s2) > 2:
+            id = s1[2] + 1
+            while id < s2[2]:
+                Pkt = self.PktMethod.GetPacketDetails(packet=Check['Pkt'][0], value=Check['Pkt'][1], limit=[id, s2[2]])
+                if len(Pkt) > 2:
+                    pktcount += 1
+ #                   res.append([f"PTx sent {Check['Pkt'][0]} data packet at index@ {Pkt[2]}", 'Pass'])
+                    id = Pkt[2] + 1
+                else:
+                    break
+            res.append([f'TPR sent {Check["Pkt"][0]}_{Check["Pkt"][1]} data packet {pktcount} times, test executed for {round(s2[0]-s1[0],3)} Secs. Limit: at least 5 responses within 1 minute', 'Pass' if s2[0]-s1[0] <= 60 or pktcount < 5 else 'Inconclusive'])
+        else:
+            if len(s1) <= 2 or len(s2) <= 2:
+                res.append(['Test_Status assertions are not found in the capture', 'Inconclusive'])
+
         return res
 
     def RP_Response(self,CTSCheck,Check,flows,flwID):
